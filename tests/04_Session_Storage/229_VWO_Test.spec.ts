634470bcee22da9fe0e38d3cd97854aeeda8122b
DevilPrajwal <prajwalbelkhode216@gmail.com>
2026-09-08 07:32:18 +0530

    Allure report lab-ex rename

--- tests/04_Session_Storage/229_VWO_Test.spec.ts
+++ tests/04_Session_Storage/229_VWO_Test.spec.ts
@@ -10,10 +10,12 @@ test("go directly to the dashboard, no login", async({ page }) =>{
     await page.goto("https://app.wingify.com/#/dashboard?accountId=1275982");
     await expect(page).toHaveURL('/dashboard/');
     console.log("Dashboard loaded - No login ✅");
+    await page.waitForTimeout(3000);
 });
 
 test(" go directly to the settings page, no login", async({ page }) =>{
     await page.goto("https://app.wingify.com/#/settings/accounts/general?accountId=1275982");
     await expect(page).toHaveURL('/settings/');
     console.log("Settings page loaded - No login ✅");
+    await page.waitForTimeout(3000);
 })
